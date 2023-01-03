2f716fd0ef07abb19e5497b16ce17eef67cb5a18
Sydney <yendev01@gmail.com>
2023-01-03 20:35:19 +0000

    prints the last digit of a number

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+def print_last_digit(number):
+    if number >= 0:
+        print("{}".format(number % 10), end="")
+        return number % 10
+    else:
+        print("{:d}".format(-number % 10), end="")
+        return (-number % 10)
