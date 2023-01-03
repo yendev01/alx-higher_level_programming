956703a63d15510a27798097e18f729502b2b30b
Sydney <yendev01@gmail.com>
2023-01-03 20:57:50 +0000

    fizzbuzz

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def fizzbuzz():
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 != 0:
+            print("{:s}".format("Fizz"), end=" ")
+        elif i % 5 == 0 and i % 3 != 0:
+            print("{:s}".format("Buzz"), end=" ")
+        elif i % 3 == 0 and i % 5 == 0:
+            print("{:s}".format("FizzBuzz"), end=" ")
+        else:
+            print("{:d}".format(i), end=" ")
