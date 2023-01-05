f38d86cd0ba4d55f99169ac1378e27c84c164ca4
Sydney <yendev01@gmail.com>
2023-01-05 17:25:38 +0000

    working with compiled module

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+import hidden_4
+
+if __name__ == '__main__':
+    files = dir(hidden_4)
+    length = len(files)
+
+    for i in range(length):
+        files[i][:2] != '__'
+        print(files[1])
