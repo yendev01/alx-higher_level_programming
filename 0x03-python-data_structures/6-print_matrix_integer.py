840ac51870e6a9d2462a07bc39742acc4679cd8d
Sydney <yendev01@gmail.com>
2023-01-08 15:25:59 +0000

    trying out something

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+def print_matrix_integer(matrix=[[]]):
+    for i in matrix:
+        length = len(i)
+        for j in range(length - 1):
+            print("{:d}".format(i[j]), end=" ")
+        if length > 0:
+            print("{}".format(i[length - 1]))
