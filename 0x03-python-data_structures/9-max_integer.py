59060021240a3259b9da4566b17a501245c01522
Sydney <yendev01@gmail.com>
2023-01-09 08:17:27 +0000

    debugging

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -4,7 +4,7 @@
 def max_integer(my_list=[]):
     if not my_list or len(my_list) == 0:
         return None
-    max_int = 0
+    max_int = my_list[0]
     for i in my_list:
         if i > max_int:
             max_int = i
