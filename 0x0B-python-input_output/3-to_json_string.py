3bf5805003b2f67dc56fa06869dae859eddcda7d
Sydney <yendev01@gmail.com>
2023-02-13 09:14:27 +0000

    debugging

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -6,5 +6,11 @@ module that contains a function to_json_string
 
 
 def to_json_string(my_obj):
-    """function that returns JSON representation of an object"""
+    """function that returns JSON representation of an object
+    Args:
+	my_obj
+
+    Raises:
+        Exception when the object can't be encoded
+    """
     return json.dumps(my_obj)
